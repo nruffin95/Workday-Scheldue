7bc92301cfae83d610354f2a232447025313a8d5
nruffin95 <nruffin95@gmail.com>
2022-04-21 23:02:16 -0500

    Added moment time display

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,5 @@
 
 // Date and Time Display
-let passDate = document.getElementById('currentDay')
-let currentDate = moment().format('dddd, MMM Do YYYY');
-let titleEl = document.getElementById('title')
 
-setInterval(function() {
-    passDate.textContent = moment().format('dddd, MMM Do YYYY');
-    if (passDate > currentDate) {
-        titleEl.style.backgroundColor = "red";
-    } 
-},
-
-
-
-
-
-
-// // Save Task Button click on function
-
-// let input = []
-
-// let saveTaskBtn = document.getElementById('btn')
-// saveTaskBtn.addEventListener('click', function(){
-    
-//     let inputTitle = document.getElementById('title').value
-//     input.push(inputTitle)
-    
-//     localStorage.setItem('Mylist', JSON.stringify(input))
-// })
-
-console.log(currentDate)
+var currentDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(currentDate);
