0bcf847601f20c1a98e3c6505d7ec934b621c342
nruffin95 <nruffin95@gmail.com>
2022-04-21 22:10:04 -0500

    Updated icon

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,32 @@
 
 // Date and Time Display
-let currentDate = document.getElementById('currentDay')
+let passDate = document.getElementById('currentDay')
+let currentDate = moment().format('dddd, MMM Do YYYY');
+let titleEl = document.getElementById('title')
+
 setInterval(function() {
-    currentDate.textContent = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-}, 1000)
-
-// Save Task Button click on function
-
-let saveTaskBtn = document.getElementById('btn')
-saveTaskBtn.addEventListener('click', function(){
-    console.log('You clicked')
-})
+    passDate.textContent = moment().format('dddd, MMM Do YYYY');
+    if (passDate > currentDate) {
+        titleEl.style.backgroundColor = "red";
+    } 
+},
 
 
+
+
+
+
+// // Save Task Button click on function
+
+// let input = []
+
+// let saveTaskBtn = document.getElementById('btn')
+// saveTaskBtn.addEventListener('click', function(){
+    
+//     let inputTitle = document.getElementById('title').value
+//     input.push(inputTitle)
+    
+//     localStorage.setItem('Mylist', JSON.stringify(input))
+// })
+
+console.log(currentDate)
